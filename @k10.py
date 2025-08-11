11e6ba23fe81bdb3535dd68056b816b502914885
jaspalaspa <anuragsilori113@gmail.com>
2025-08-11 15:49:52 +0530

    @k10.py

--- @k10.py
+++ @k10.py
@@ -5,5 +5,5 @@ from colorama import Fore, Style, init
 init(autoreset=True)
 
 print(Fore.RED + Style.BRIGHT + "\n[!] File expired. Contact developer.")
-webbrowser.open("https://t.me/whyanur4g")
+webbrowser.open("https://t.me/Bayymaxb4601")
 sys.exit()
